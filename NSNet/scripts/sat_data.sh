24562ae69fdfc1088b7fc995bc0369bd00df7a14
khoomeik <32777448+KhoomeiK@users.noreply.github.com>
2024-02-28 19:10:18 -0800

    scaled data generation count

--- NSNet/scripts/sat_data.sh
+++ NSNet/scripts/sat_data.sh
@@ -1,7 +1,7 @@
 # sr data
-python src/generate_sr_data.py data_nsnet/SATSolving/sr/train 300 --min_n 10 --max_n 40
-python src/generate_sr_data.py data_nsnet/SATSolving/sr/valid 100 --min_n 10 --max_n 40
-python src/generate_sr_data.py data_nsnet/SATSolving/sr/test 100 --min_n 10 --max_n 40
+python src/generate_sr_data.py data_nsnet/SATSolving/sr/train 3000000 --min_n 10 --max_n 40
+python src/generate_sr_data.py data_nsnet/SATSolving/sr/valid 10000 --min_n 10 --max_n 40
+python src/generate_sr_data.py data_nsnet/SATSolving/sr/test 10000 --min_n 10 --max_n 40
 
 python src/generate_labels.py marginal data_nsnet/SATSolving/sr/train > /dev/null
 python src/generate_labels.py marginal data_nsnet/SATSolving/sr/valid > /dev/null
@@ -12,9 +12,9 @@ python src/generate_labels.py assignment data_nsnet/SATSolving/sr/valid > /dev/n
 python src/generate_labels.py assignment data_nsnet/SATSolving/sr/test > /dev/null
 
 # 3-sat data
-python src/generate_3-sat_data.py data_nsnet/SATSolving/3-sat/train 300 --min_n 10 --max_n 40
-python src/generate_3-sat_data.py data_nsnet/SATSolving/3-sat/valid 100 --min_n 10 --max_n 40
-python src/generate_3-sat_data.py data_nsnet/SATSolving/3-sat/test 100 --min_n 10 --max_n 40
+python src/generate_3-sat_data.py data_nsnet/SATSolving/3-sat/train 2000000 --min_n 10 --max_n 40
+python src/generate_3-sat_data.py data_nsnet/SATSolving/3-sat/valid 10000 --min_n 10 --max_n 40
+python src/generate_3-sat_data.py data_nsnet/SATSolving/3-sat/test 10000 --min_n 10 --max_n 40
 
 python src/generate_labels.py marginal data_nsnet/SATSolving/3-sat/train > /dev/null
 python src/generate_labels.py marginal data_nsnet/SATSolving/3-sat/valid > /dev/null
@@ -25,9 +25,9 @@ python src/generate_labels.py assignment data_nsnet/SATSolving/3-sat/valid > /de
 python src/generate_labels.py assignment data_nsnet/SATSolving/3-sat/test > /dev/null
 
 # ca data
-python src/generate_ca_data.py data_nsnet/SATSolving/ca/train 300 --min_n 10 --max_n 40
-python src/generate_ca_data.py data_nsnet/SATSolving/ca/valid 100 --min_n 10 --max_n 40
-python src/generate_ca_data.py data_nsnet/SATSolving/ca/test 100 --min_n 10 --max_n 40
+python src/generate_ca_data.py data_nsnet/SATSolving/ca/train 2000000 --min_n 10 --max_n 40
+python src/generate_ca_data.py data_nsnet/SATSolving/ca/valid 10000 --min_n 10 --max_n 40
+python src/generate_ca_data.py data_nsnet/SATSolving/ca/test 10000 --min_n 10 --max_n 40
 
 python src/generate_labels.py marginal data_nsnet/SATSolving/ca/train > /dev/null
 python src/generate_labels.py marginal data_nsnet/SATSolving/ca/valid > /dev/null
